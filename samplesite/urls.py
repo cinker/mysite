be1acc9cb0df296b3782a31c873f15174df40917
Teng <cdoors@gmail.com>
2020-01-04 02:29:39 +0000

    refresh

--- samplesite/urls.py
+++ samplesite/urls.py
@@ -1,7 +1,7 @@
-"""mysite URL Configuration
+"""samplesite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,18 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import RedirectView
-# Use static() to add url mapping to serve static files during development (only)
-from django.conf import settings
-from django.conf.urls.static import static
-
-
+from django.urls import path
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/fund/', permanent=True)),
-    path('fund/', include('fundapp.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
-
